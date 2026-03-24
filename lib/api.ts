4eecf0eb8c92044d9d6b5c919aa8373b996ccd42
Manuel Torres <matt1507.mt@gmail.com>
2026-03-24 14:23:23 -0500

    ddns al server

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, getUser } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://98.90.9.90/api/v1",
+  baseURL: "https://vitalia.ddns.net/api/v1",
 });
 
 api.interceptors.request.use(
